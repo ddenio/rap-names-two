9037868ae749d9bfdba2a82fdabb48da092dfa4b
Derin DeNio <derindenio@gmail.com>
2024-02-07 16:39:48 -0700

    Fixed Cors error

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 //modules
 const express = require('express')
 const app = express()
+const cors = require('cors')
 const PORT = process.env.PORT || 8000
+
+app.use(cors())
 
 const rappers = {
     '21 savage':{
